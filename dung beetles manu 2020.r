ee8c740678e257c31b4cc56d4e4021833775cb35
laurajallen <laurajallen@hotmail.com>
2020-10-10 11:24:00 +0100

    outlined plan for revised analysis

---
+++ dung beetles manu 2020.r
@@ -0,0 +1,15 @@
+## Manu dung beetles
+## Laura  Allen - 10/10/2020
+
+## Key questions:
+## 1) How does the alpha diversity profile change across disturbance gradient
+## 2) How does species composition change (RDA) 
+## 3) Does ratio of abundance of large: small beetles change?
+## 4) does ratio of abundance of rollers:tunnellers change?
+## 5) does the dispersal of small+medium seeds change along the gradient (large seeds = control?).
+
+## other necessary points:
+## veg pca to show disturbance
+## morans i for spatial autocorrelation?
+
+## NExt: start importing the analysis step by step to address the specific issues.
